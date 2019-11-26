4ed1628002a0350abc3751dcd02164163a939222
Vasilis <799453724@qq.com>
2019-11-26 11:11:52 +0800

    为所有指令增加tab补全功能

--- civcraft/src/main/java/com/avrgaming/civcraft/command/camp/CampCommand.java
+++ civcraft/src/main/java/com/avrgaming/civcraft/command/camp/CampCommand.java
@@ -1,11 +1,11 @@
 /*************************************************************************
- * 
+ *
  * AVRGAMING LLC
  * __________________
- * 
+ *
  *  [2013] AVRGAMING LLC
  *  All Rights Reserved.
- * 
+ *
  * NOTICE:  All information contained herein is, and remains
  * the property of AVRGAMING LLC and its suppliers,
  * if any.  The intellectual and technical concepts contained
@@ -18,13 +18,6 @@
  */
 package com.avrgaming.civcraft.command.camp;
 
-import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.HashMap;
-
-import org.bukkit.entity.Player;
-import org.bukkit.inventory.ItemStack;
-
 import com.avrgaming.civcraft.camp.Camp;
 import com.avrgaming.civcraft.command.CommandBase;
 import com.avrgaming.civcraft.config.CivSettings;
@@ -35,239 +28,243 @@ import com.avrgaming.civcraft.main.CivMessage;
 import com.avrgaming.civcraft.object.Resident;
 import com.avrgaming.civcraft.questions.JoinCampResponse;
 import com.avrgaming.civcraft.util.CivColor;
+import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
-public class CampCommand extends CommandBase {
-	public static final long INVITE_TIMEOUT = 30000; //30 seconds
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.HashMap;
 
-	@Override
-	public void init() {
-		command = "/camp";
-		displayName = CivSettings.localize.localizedString("Camp");
-		
-		commands.put("undo", CivSettings.localize.localizedString("cmd_camp_undoDesc"));
-		commands.put("add", CivSettings.localize.localizedString("cmd_camp_addDesc"));
-		commands.put("remove", CivSettings.localize.localizedString("cmd_camp_removeDesc"));
-		commands.put("leave", CivSettings.localize.localizedString("cmd_camp_leaveDesc"));
-		commands.put("setowner", CivSettings.localize.localizedString("cmd_camp_setownerDesc"));
-		commands.put("info", CivSettings.localize.localizedString("cmd_camp_infoDesc"));
-		commands.put("disband", CivSettings.localize.localizedString("cmd_camp_disbandDesc"));
-		commands.put("upgrade", CivSettings.localize.localizedString("cmd_camp_upgradeDesc"));
-		commands.put("refresh", CivSettings.localize.localizedString("cmd_camp_refreshDesc"));
-		commands.put("location", CivSettings.localize.localizedString("cmd_camp_locationDesc"));
-	}
-	
-	public void location_cmd() throws CivException {
-		Resident resident = getResident();
-		
-		if (!resident.hasCamp()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
-		}
-		Camp camp = resident.getCamp();
+public class CampCommand extends CommandBase{
+    public static final long INVITE_TIMEOUT = 30000; //30 seconds
 
-        if (camp != null) 
-        {
-                CivMessage.send(sender, "");
-                CivMessage.send(sender, CivColor.LightGreen+CivColor.BOLD+CivSettings.localize.localizedString("cmd_camp_locationSuccess")+" "+CivColor.LightPurple+camp.getCorner());
-                CivMessage.send(sender, "");
+    @Override
+    public void init() {
+        command = "/camp";
+        displayName = CivSettings.localize.localizedString("Camp");
+
+        commands.put("undo", CivSettings.localize.localizedString("cmd_camp_undoDesc"));
+        commands.put("add", CivSettings.localize.localizedString("cmd_camp_addDesc"));
+        commands.put("remove", CivSettings.localize.localizedString("cmd_camp_removeDesc"));
+        commands.put("leave", CivSettings.localize.localizedString("cmd_camp_leaveDesc"));
+        commands.put("setowner", CivSettings.localize.localizedString("cmd_camp_setownerDesc"));
+        commands.put("info", CivSettings.localize.localizedString("cmd_camp_infoDesc"));
+        commands.put("disband", CivSettings.localize.localizedString("cmd_camp_disbandDesc"));
+        commands.put("upgrade", CivSettings.localize.localizedString("cmd_camp_upgradeDesc"));
+        commands.put("refresh", CivSettings.localize.localizedString("cmd_camp_refreshDesc"));
+        commands.put("location", CivSettings.localize.localizedString("cmd_camp_locationDesc"));
+    }
+
+    public void location_cmd() throws CivException {
+        Resident resident = getResident();
+
+        if (!resident.hasCamp()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
+        }
+        Camp camp = resident.getCamp();
+
+        if (camp != null) {
+            CivMessage.send(sender, "");
+            CivMessage.send(sender, CivColor.LightGreen + CivColor.BOLD + CivSettings.localize.localizedString("cmd_camp_locationSuccess") + " " + CivColor.LightPurple + camp.getCorner());
+            CivMessage.send(sender, "");
         }
     }
-	
-	
-	public void refresh_cmd() throws CivException {
-		Resident resident = getResident();
-		
-		if (!resident.hasCamp()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
-		}
-		
-		Camp camp = resident.getCamp();
-		if (camp.getOwner() != resident) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_refreshNotOwner"));
-		}
-		
-		if (camp.isDestroyed())
-		{
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_refreshDestroyed"));
-		}
-		
-		try {
-			camp.repairFromTemplate();
-		} catch (IOException e) {
-		} catch (CivException e) {
-			e.printStackTrace();
-		}
-		camp.reprocessCommandSigns();
-		CivMessage.send(sender, CivSettings.localize.localizedString("cmd_camp_refreshSuccess"));
-	}
-	
-	public void upgrade_cmd() {
-		CampUpgradeCommand cmd = new CampUpgradeCommand();	
-		cmd.onCommand(sender, null, "camp", this.stripArgs(args, 1));
-	}
-	
-	public void info_cmd() throws CivException {
-		Camp camp = this.getCurrentCamp();
-		SimpleDateFormat sdf = new SimpleDateFormat("M/dd h:mm:ss a z");
 
-		CivMessage.sendHeading(sender, CivSettings.localize.localizedString("var_camp_infoHeading",camp.getName()));
-		HashMap<String,String> info = new HashMap<String, String>();
-		info.put(CivSettings.localize.localizedString("Owner"), camp.getOwnerName());
-		info.put(CivSettings.localize.localizedString("Members"), ""+camp.getMembers().size());
-		info.put(CivSettings.localize.localizedString("NextRaid"), ""+sdf.format(camp.getNextRaidDate()));
-		CivMessage.send(sender, this.makeInfoString(info, CivColor.Green, CivColor.LightGreen));
-		
-		info.clear();
-		info.put(CivSettings.localize.localizedString("cmd_camp_infoFireLeft"), ""+camp.getFirepoints());
-		info.put(CivSettings.localize.localizedString("cmd_camp_infoLonghouseLevel"), ""+camp.getLonghouseLevel()+""+camp.getLonghouseCountString());
-		CivMessage.send(sender, this.makeInfoString(info, CivColor.Green, CivColor.LightGreen));
 
-		info.clear();
-		info.put(CivSettings.localize.localizedString("Members"), camp.getMembersString());
-		CivMessage.send(sender, this.makeInfoString(info, CivColor.Green, CivColor.LightGreen));
-	}
-	
-	public void remove_cmd() throws CivException {
-		this.validCampOwner();
-		Camp camp = getCurrentCamp();
-		Resident resident = getNamedResident(1);
-		
-		if (!resident.hasCamp() || resident.getCamp() != camp) {
-			throw new CivException(CivSettings.localize.localizedString("var_cmd_camp_removeNotInCamp",resident.getName()));
-		}
-		
-		if (resident.getCamp().getOwner() == resident) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_removeErrorOwner"));
-		}
-		
-		camp.removeMember(resident);
-		CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_removeSuccess",resident.getName()));
-	}
-	
-	public void add_cmd() throws CivException {
-		this.validCampOwner();
-		Camp camp = this.getCurrentCamp();
-		Resident resident = getNamedResident(1);
-		Player player = getPlayer();
-		
-		if (resident.hasCamp()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_addInCamp"));
-		}
-		
-		if (resident.hasTown()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_addInTown"));
-		}
-		
-		JoinCampResponse join = new JoinCampResponse();
-		join.camp = camp;
-		join.resident = resident;
-		join.sender = player;
-		
-		CivGlobal.questionPlayer(player, CivGlobal.getPlayer(resident), 
-				CivSettings.localize.localizedString("var_cmd_camp_addInvite",player.getName(),camp.getName()),
-				INVITE_TIMEOUT, join);
-		
-		CivMessage.sendSuccess(player, CivSettings.localize.localizedString("var_cmd_camp_addSuccess",resident.getName()));
-	}
-	
-	public void setowner_cmd() throws CivException {
-		this.validCampOwner();
-		Camp camp = getCurrentCamp();
-		Resident newLeader = getNamedResident(1);
-		
-		if (!camp.hasMember(newLeader.getName())) {
-			throw new CivException(CivSettings.localize.localizedString("var_cmd_camp_removeNotInCamp",newLeader.getName()));
-		}
-		
-		camp.setOwner(newLeader);
-		camp.save();
-		
-		Player player = CivGlobal.getPlayer(newLeader);
-		CivMessage.sendSuccess(player, CivSettings.localize.localizedString("var_cmd_camp_setownerMsg",camp.getName()));
-		CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_setownerSuccess",newLeader.getName()));
-		
-	}
-	
-	public void leave_cmd() throws CivException {
-		Resident resident = getResident();
-		
-		if (!resident.hasCamp()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
-		}
-		
-		Camp camp = resident.getCamp();
-		if (camp.getOwner() == resident) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_leaveOwner"));
-		}
-		
-		camp.removeMember(resident);
-		camp.save();
-		CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_leaveSuccess",camp.getName()));
-	}
-	
-	public void new_cmd() throws CivException {
+    public void refresh_cmd() throws CivException {
+        Resident resident = getResident();
 
-	}
-	
-	public void disband_cmd() throws CivException {
-		Resident resident = getResident();
-		this.validCampOwner();
-		Camp camp = this.getCurrentCamp();
-		
-		if (!resident.hasCamp()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
-		}
+        if (!resident.hasCamp()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
+        }
 
-		camp.disband();
-		CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("cmd_camp_disbandSuccess"));
-	}
-	
-	public void undo_cmd() throws CivException {
-		Resident resident = getResident();
-		
-		if (!resident.hasCamp()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
-		}
-		
-		Camp camp = resident.getCamp();
-		if (camp.getOwner() != resident) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_undoNotOwner"));
-		}
-		
-		if (!camp.isUndoable()) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_undoTooLate"));
-		}
-		
-		LoreCraftableMaterial campMat = LoreCraftableMaterial.getCraftMaterialFromId("mat_found_camp");
-		if (campMat == null) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_undoError"));
-		}
-		
-		ItemStack newStack = LoreCraftableMaterial.spawn(campMat);
-		Player player = CivGlobal.getPlayer(resident);
-		HashMap<Integer, ItemStack> leftovers = player.getInventory().addItem(newStack);
-		for (ItemStack stack : leftovers.values()) {
-			player.getWorld().dropItem(player.getLocation(), stack);
-			CivMessage.send(player, CivColor.LightGray+CivSettings.localize.localizedString("cmd_camp_undoFullInven"));
-		}
-		
-		camp.undo();
-		CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("cmd_camp_undoSuccess"));
-		
-	}
-	
+        Camp camp = resident.getCamp();
+        if (camp.getOwner() != resident) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_refreshNotOwner"));
+        }
 
-	@Override
-	public void doDefaultAction() throws CivException {
-		showHelp();
-	}
+        if (camp.isDestroyed()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_refreshDestroyed"));
+        }
 
-	@Override
-	public void showHelp() {
-		showBasicHelp();
-	}
+        try {
+            camp.repairFromTemplate();
+        } catch (IOException e) {
+        } catch (CivException e) {
+            e.printStackTrace();
+        }
+        camp.reprocessCommandSigns();
+        CivMessage.send(sender, CivSettings.localize.localizedString("cmd_camp_refreshSuccess"));
+    }
 
-	@Override
-	public void permissionCheck() throws CivException {
-	}
+    public void upgrade_cmd() {
+        CampUpgradeCommand cmd = new CampUpgradeCommand();
+        cmd.onCommand(sender, null, "camp", this.stripArgs(args, 1));
+    }
+
+    public void info_cmd() throws CivException {
+        Camp camp = this.getCurrentCamp();
+        SimpleDateFormat sdf = new SimpleDateFormat("M/dd h:mm:ss a z");
+
+        CivMessage.sendHeading(sender, CivSettings.localize.localizedString("var_camp_infoHeading", camp.getName()));
+        HashMap<String, String> info = new HashMap<String, String>();
+        info.put(CivSettings.localize.localizedString("Owner"), camp.getOwnerName());
+        info.put(CivSettings.localize.localizedString("Members"), "" + camp.getMembers().size());
+        info.put(CivSettings.localize.localizedString("NextRaid"), "" + sdf.format(camp.getNextRaidDate()));
+        CivMessage.send(sender, this.makeInfoString(info, CivColor.Green, CivColor.LightGreen));
+
+        info.clear();
+        info.put(CivSettings.localize.localizedString("cmd_camp_infoFireLeft"), "" + camp.getFirepoints());
+        info.put(CivSettings.localize.localizedString("cmd_camp_infoLonghouseLevel"), "" + camp.getLonghouseLevel() + "" + camp.getLonghouseCountString());
+        CivMessage.send(sender, this.makeInfoString(info, CivColor.Green, CivColor.LightGreen));
+
+        info.clear();
+        info.put(CivSettings.localize.localizedString("Members"), camp.getMembersString());
+        CivMessage.send(sender, this.makeInfoString(info, CivColor.Green, CivColor.LightGreen));
+    }
+
+    public void remove_cmd() throws CivException {
+        this.validCampOwner();
+        Camp camp = getCurrentCamp();
+        Resident resident = getNamedResident(1);
+
+        if (!resident.hasCamp() || resident.getCamp() != camp) {
+            throw new CivException(CivSettings.localize.localizedString("var_cmd_camp_removeNotInCamp", resident.getName()));
+        }
+
+        if (resident.getCamp().getOwner() == resident) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_removeErrorOwner"));
+        }
+
+        camp.removeMember(resident);
+        CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_removeSuccess", resident.getName()));
+    }
+
+    public void add_cmd() throws CivException {
+        this.validCampOwner();
+        Camp camp = this.getCurrentCamp();
+        Resident resident = getNamedResident(1);
+        Player player = getPlayer();
+
+        if (resident.hasCamp()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_addInCamp"));
+        }
+
+        if (resident.hasTown()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_addInTown"));
+        }
+
+        JoinCampResponse join = new JoinCampResponse();
+        join.camp = camp;
+        join.resident = resident;
+        join.sender = player;
+
+        CivGlobal.questionPlayer(player, CivGlobal.getPlayer(resident),
+                CivSettings.localize.localizedString("var_cmd_camp_addInvite", player.getName(), camp.getName()),
+                INVITE_TIMEOUT, join);
+
+        CivMessage.sendSuccess(player, CivSettings.localize.localizedString("var_cmd_camp_addSuccess", resident.getName()));
+    }
+
+    public void setowner_cmd() throws CivException {
+        this.validCampOwner();
+        Camp camp = getCurrentCamp();
+        Resident newLeader = getNamedResident(1);
+
+        if (!camp.hasMember(newLeader.getName())) {
+            throw new CivException(CivSettings.localize.localizedString("var_cmd_camp_removeNotInCamp", newLeader.getName()));
+        }
+
+        camp.setOwner(newLeader);
+        camp.save();
+
+        Player player = CivGlobal.getPlayer(newLeader);
+        CivMessage.sendSuccess(player, CivSettings.localize.localizedString("var_cmd_camp_setownerMsg", camp.getName()));
+        CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_setownerSuccess", newLeader.getName()));
+
+    }
+
+    public void leave_cmd() throws CivException {
+        Resident resident = getResident();
+
+        if (!resident.hasCamp()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
+        }
+
+        Camp camp = resident.getCamp();
+        if (camp.getOwner() == resident) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_leaveOwner"));
+        }
+
+        camp.removeMember(resident);
+        camp.save();
+        CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_leaveSuccess", camp.getName()));
+    }
+
+    public void new_cmd() throws CivException {
+
+    }
+
+    public void disband_cmd() throws CivException {
+        Resident resident = getResident();
+        this.validCampOwner();
+        Camp camp = this.getCurrentCamp();
+
+        if (!resident.hasCamp()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
+        }
+
+        camp.disband();
+        CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("cmd_camp_disbandSuccess"));
+    }
+
+    public void undo_cmd() throws CivException {
+        Resident resident = getResident();
+
+        if (!resident.hasCamp()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_campBase_NotInCamp"));
+        }
+
+        Camp camp = resident.getCamp();
+        if (camp.getOwner() != resident) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_undoNotOwner"));
+        }
+
+        if (!camp.isUndoable()) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_undoTooLate"));
+        }
+
+        LoreCraftableMaterial campMat = LoreCraftableMaterial.getCraftMaterialFromId("mat_found_camp");
+        if (campMat == null) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_undoError"));
+        }
+
+        ItemStack newStack = LoreCraftableMaterial.spawn(campMat);
+        Player player = CivGlobal.getPlayer(resident);
+        HashMap<Integer, ItemStack> leftovers = player.getInventory().addItem(newStack);
+        for (ItemStack stack : leftovers.values()) {
+            player.getWorld().dropItem(player.getLocation(), stack);
+            CivMessage.send(player, CivColor.LightGray + CivSettings.localize.localizedString("cmd_camp_undoFullInven"));
+        }
+
+        camp.undo();
+        CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("cmd_camp_undoSuccess"));
+
+    }
+
+
+    @Override
+    public void doDefaultAction() throws CivException {
+        showHelp();
+    }
+
+    @Override
+    public void showHelp() {
+        showBasicHelp();
+    }
+
+    @Override
+    public void permissionCheck() throws CivException {
+    }
 
 }
--- civcraft/src/main/java/com/avrgaming/civcraft/command/camp/CampUpgradeCommand.java
+++ civcraft/src/main/java/com/avrgaming/civcraft/command/camp/CampUpgradeCommand.java
@@ -1,11 +1,11 @@
 /*************************************************************************
- * 
+ *
  * AVRGAMING LLC
  * __________________
- * 
+ *
  *  [2013] AVRGAMING LLC
  *  All Rights Reserved.
- * 
+ *
  * NOTICE:  All information contained herein is, and remains
  * the property of AVRGAMING LLC and its suppliers,
  * if any.  The intellectual and technical concepts contained
@@ -25,89 +25,105 @@ import com.avrgaming.civcraft.config.ConfigCampUpgrade;
 import com.avrgaming.civcraft.exception.CivException;
 import com.avrgaming.civcraft.main.CivMessage;
 import com.avrgaming.civcraft.util.CivColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
-public class CampUpgradeCommand extends CommandBase {
-	@Override
-	public void init() {
-		command = "/camp upgrade";
-		displayName = CivSettings.localize.localizedString("cmd_camp_upgrade_name");
-		
-		
-		commands.put("list", CivSettings.localize.localizedString("cmd_camp_upgrade_listDesc"));
-		commands.put("purchased", CivSettings.localize.localizedString("cmd_camp_upgrade_purchasedDesc"));
-		commands.put("buy", CivSettings.localize.localizedString("cmd_camp_upgrade_buyDesc"));
-		
-	}
+import java.util.ArrayList;
+import java.util.List;
 
-	public void purchased_cmd() throws CivException {
-		Camp camp = this.getCurrentCamp();
-		CivMessage.sendHeading(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_purchasedSuccess"));
+public class CampUpgradeCommand extends CommandBase implements TabExecutor {
+    @Override
+    public void init() {
+        command = "/camp upgrade";
+        displayName = CivSettings.localize.localizedString("cmd_camp_upgrade_name");
 
-		String out = "";
-		for (ConfigCampUpgrade upgrade : camp.getUpgrades()) {
-			out += upgrade.name+", ";
-		}
-		
-		CivMessage.send(sender, out);
-	}
-	
-	private void list_upgrades(Camp camp) throws CivException {				
-		for (ConfigCampUpgrade upgrade : CivSettings.campUpgrades.values()) {
-			if (upgrade.isAvailable(camp)) {
-				CivMessage.send(sender, upgrade.name+" "+CivColor.LightGray+CivSettings.localize.localizedString("Cost")+" "+CivColor.Yellow+upgrade.cost);
-			}
-		}
-	}
-	
-	public void list_cmd() throws CivException {
-		Camp camp = this.getCurrentCamp();
 
-		CivMessage.sendHeading(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_list"));	
-		list_upgrades(camp);		
-	}
-	
-	public void buy_cmd() throws CivException {
-		Camp camp = this.getCurrentCamp();
+        commands.put("list", CivSettings.localize.localizedString("cmd_camp_upgrade_listDesc"));
+        commands.put("purchased", CivSettings.localize.localizedString("cmd_camp_upgrade_purchasedDesc"));
+        commands.put("buy", CivSettings.localize.localizedString("cmd_camp_upgrade_buyDesc"));
 
-		if (args.length < 2) {
-			CivMessage.sendHeading(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_list"));
-			list_upgrades(camp);		
-			CivMessage.send(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_buyHeading"));
-			return;
-		}
-				
-		String combinedArgs = "";
-		args = this.stripArgs(args, 1);
-		for (String arg : args) {
-			combinedArgs += arg + " ";
-		}
-		combinedArgs = combinedArgs.trim();
-		
-		ConfigCampUpgrade upgrade = CivSettings.getCampUpgradeByNameRegex(camp, combinedArgs);
-		if (upgrade == null) {
-			throw new CivException(CivSettings.localize.localizedString("var_cmd_camp_upgrade_buyInvalid",combinedArgs));
-		}
-		
-		if (camp.hasUpgrade(upgrade.id)) {
-			throw new CivException(CivSettings.localize.localizedString("cmd_camp_upgrade_buyOwned"));
-		}
-		
-		camp.purchaseUpgrade(upgrade);
-		CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_upgrade_buySuccess",upgrade.name));
-	}
-	
-	@Override
-	public void doDefaultAction() throws CivException {
-		showHelp();
-	}
+    }
 
-	@Override
-	public void showHelp() {
-		showBasicHelp();
-	}
+    public void purchased_cmd() throws CivException {
+        Camp camp = this.getCurrentCamp();
+        CivMessage.sendHeading(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_purchasedSuccess"));
 
-	@Override
-	public void permissionCheck() throws CivException {	
-		this.validCampOwner();
-	}
+        String out = "";
+        for (ConfigCampUpgrade upgrade : camp.getUpgrades()) {
+            out += upgrade.name + ", ";
+        }
+
+        CivMessage.send(sender, out);
+    }
+
+    private void list_upgrades(Camp camp) throws CivException {
+        for (ConfigCampUpgrade upgrade : CivSettings.campUpgrades.values()) {
+            if (upgrade.isAvailable(camp)) {
+                CivMessage.send(sender, upgrade.name + " " + CivColor.LightGray + CivSettings.localize.localizedString("Cost") + " " + CivColor.Yellow + upgrade.cost);
+            }
+        }
+    }
+
+    public void list_cmd() throws CivException {
+        Camp camp = this.getCurrentCamp();
+        CivMessage.sendHeading(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_list"));
+        list_upgrades(camp);
+    }
+
+    public void buy_cmd() throws CivException {
+        Camp camp = this.getCurrentCamp();
+
+        if (args.length < 2) {
+            CivMessage.sendHeading(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_list"));
+            list_upgrades(camp);
+            CivMessage.send(sender, CivSettings.localize.localizedString("cmd_camp_upgrade_buyHeading"));
+            return;
+        }
+
+        String combinedArgs = "";
+        args = this.stripArgs(args, 1);
+        for (String arg : args) {
+            combinedArgs += arg + " ";
+        }
+        combinedArgs = combinedArgs.trim();
+
+        ConfigCampUpgrade upgrade = CivSettings.getCampUpgradeByNameRegex(camp, combinedArgs);
+        if (upgrade == null) {
+            throw new CivException(CivSettings.localize.localizedString("var_cmd_camp_upgrade_buyInvalid", combinedArgs));
+        }
+
+        if (camp.hasUpgrade(upgrade.id)) {
+            throw new CivException(CivSettings.localize.localizedString("cmd_camp_upgrade_buyOwned"));
+        }
+
+        camp.purchaseUpgrade(upgrade);
+        CivMessage.sendSuccess(sender, CivSettings.localize.localizedString("var_cmd_camp_upgrade_buySuccess", upgrade.name));
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        if (args.length == 2 && "buy".equalsIgnoreCase(args[0])) {
+            //return
+        }
+        super.onTabComplete(sender, command, alias, args);
+        return new ArrayList<>();
+    }
+
+
+    @Override
+    public void doDefaultAction() throws CivException {
+        showHelp();
+    }
+
+
+    @Override
+    public void showHelp() {
+        showBasicHelp();
+    }
+
+    @Override
+    public void permissionCheck() throws CivException {
+        this.validCampOwner();
+    }
 }
