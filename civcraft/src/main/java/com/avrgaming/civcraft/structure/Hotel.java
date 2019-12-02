d8077a56653b83467742c68bb80b5c1bf957995a
Vasilis <799453724@qq.com>
2019-12-02 11:55:22 +0800

    更新resource/data的汉化

--- civcraft/src/main/java/com/avrgaming/civcraft/structure/Hotel.java
+++ civcraft/src/main/java/com/avrgaming/civcraft/structure/Hotel.java
@@ -1,16 +1,14 @@
 
 package com.avrgaming.civcraft.structure;
 
+import com.avrgaming.civcraft.exception.CivException;
+import com.avrgaming.civcraft.object.Town;
+import org.bukkit.Location;
+
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
-import org.bukkit.Location;
-import com.avrgaming.civcraft.exception.CivException;
-import com.avrgaming.civcraft.object.Town;
-import com.avrgaming.civcraft.structure.Structure;
-
-public class Hotel
-        extends Structure {
+public class Hotel extends Structure {
     protected Hotel(Location center, String id, Town town) throws CivException {
         super(center, id, town);
     }
