b680a59ea925a4a29768dea9dadd46ee2f8c5e42
kun <0kun0@users.noreply.github.com>
2020-02-23 13:36:15 +0800

    修复怪物攻击力设置null

--- civcraft/src/main/java/com/avrgaming/civcraft/config/ConfigMobs.java
+++ civcraft/src/main/java/com/avrgaming/civcraft/config/ConfigMobs.java
@@ -3,6 +3,7 @@ package com.avrgaming.civcraft.config;
 import com.avrgaming.civcraft.main.CivLog;
 import org.bukkit.attribute.Attributable;
 import org.bukkit.attribute.Attribute;
+import org.bukkit.attribute.AttributeInstance;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity;
 import org.bukkit.entity.Damageable;
@@ -84,7 +85,10 @@ public class ConfigMobs {
     }
 
     public void setAttack(CraftEntity ent, double attack) {
-        ((Attributable) ent).getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(attack);
+        AttributeInstance attribute = ((Attributable) ent).getAttribute(Attribute.GENERIC_ATTACK_DAMAGE);
+        if(attribute!=null) {
+            attribute.setBaseValue(attack);
+        }
     }
 
     public void setDefense(CraftEntity ent, double defense) {
